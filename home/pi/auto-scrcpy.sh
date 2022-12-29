6ceec644aa0b6f9667c942cf6947436abfa6b0df
NoDiskNoFun <luke-xd@web.de>
2022-12-29 15:18:01 +0100

    Update auto-scrcpy.sh

--- home/pi/auto-scrcpy.sh
+++ home/pi/auto-scrcpy.sh
@@ -3,5 +3,12 @@
 while [ "1" = "1" ]
 do
 	adb wait-for-device
+	
+        bash /home/pi/screensaveroff.sh &
+
+        export DISPLAY=:0
+        export XAUTHORITY=/home/pi/.Xauthority
+
+	
 	scrcpy --always-on-top -f --max-size=1280
 done
